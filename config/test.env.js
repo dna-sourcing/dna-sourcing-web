3dbcc84f83117a7af56368a029a4237422e8f5b3
ericyang <ericyangchen@foxmail.com>
2020-01-03 14:52:29 +0800

    test config

--- config/test.env.js
+++ config/test.env.js
@@ -4,7 +4,7 @@ const devEnv = require("./dev.env");
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"test"',
-  API_ROOT: '"http://106.75.237.118:7071/"',
+  API_ROOT: '"http://113.31.111.76:7071/"',
   fileUrl: '"/static/dna-sourcing-template.xlsx"',
   ONTID_SIGININ: '"http://139.219.136.188:10390?params="',
   TOC_API_ROOT: '"http://172.168.3.68:9088/"',
