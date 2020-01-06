6c2a92724555ed995dc86613d4075567080cf8a5
ericyang <ericyangchen@foxmail.com>
2020-01-06 17:45:04 +0800

    pro cong

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT:'"http://42.159.142.241:7071/"',
-  fileUrl:'"/static/dna-sourcing-template.xlsx"',
+  API_ROOT: '"http://10.200.37.84:7071/"',
+  fileUrl: '"/static/dna-sourcing-template.xlsx"',
   ONTID_SIGININ: '"https://signin.ont.io?params="',
   APP_ONTID: '"did:ont:APeE1piJrhsETgpngHsT3hmuvrjAiDQt4N"',
-  TOC_API_ROOT: '"http://42.159.142.241:7071/"',
+  TOC_API_ROOT: '"http://10.200.37.84:7071/"',
   JWT_API: '"http://172.168.3.68:7091"'
-}
+};
